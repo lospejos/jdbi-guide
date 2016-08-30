acd3cc5a73b100b9ee9cc8ff5b2767f75aab8950
James Brunskill <james.brunskill@gmail.com>
2016-08-30 23:12:46 +1200

    Added missing bind param for put request
    The bind parameter to update the pet name was missing from code and
    documentation. This caused an error at run time. Also added test for
    this case to tests.

--- src/test/java/restfuljdbi/AppTest.java
+++ src/test/java/restfuljdbi/AppTest.java
@@ -12,9 +12,14 @@ public class AppTest extends BaseTest {
     server.post("/pets")
         .body("{\"name\": \"Oliver\"}", "application/json")
         .expect("{\"id\":1,\"name\":\"Oliver\"}");
-
+    
     server.get("/pets")
-        .expect("[{\"id\":1,\"name\":\"Oliver\"}]");
-  }
+    .expect("[{\"id\":1,\"name\":\"Oliver\"}]");
+     
+    server.put("/pets")
+	    .body("{\"id\": 1, \"name\": \"Jemima\"}", "application/json")
+	    .expect("{\"id\":1,\"name\":\"Jemima\"}");
 
+  }
+  
 }
