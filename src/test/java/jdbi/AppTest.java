220bd83fdf41e0e44072ccfdbca4bd8dfd785692
Rustam <ismailov.rustam@gmail.com>
2018-06-24 20:27:45 +0300

    Replaced magic values with constants, added default JVM args in build script

--- src/test/java/jdbi/AppTest.java
+++ src/test/java/jdbi/AppTest.java
@@ -4,6 +4,8 @@ import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.jooby.test.JoobyRule;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -21,7 +23,7 @@ public class AppTest {
     @Test
     public void pets() throws Exception {
         given()
-                .contentType("application/json")
+                .contentType(ContentType.JSON /* "application/json"*/)
                 .body("{\"name\": \"Oliver\"}")
                 .post(APP_PATH)
                 .then()
@@ -39,7 +41,7 @@ public class AppTest {
                 .body(equalTo("{\"id\":1,\"name\":\"Oliver\"}"));
 
         given()
-                .contentType("application/json")
+                .contentType(ContentType.JSON /*"application/json"*/)
                 .body("{\"id\": 1, \"name\": \"Jemima\"}")
                 .put(APP_PATH)
                 .then()
@@ -50,6 +52,6 @@ public class AppTest {
                 .delete(APP_PATH + "/1")
                 .then()
                 .assertThat()
-                .statusCode(204);
+                .statusCode(HttpStatus.SC_NO_CONTENT /*204*/);
     }
 }
